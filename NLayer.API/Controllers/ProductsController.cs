2f37d65ca239496686f0522380da92893208353d
Ferhat Cakmakoglu <ferhat.cakmakoglu61@gmail.com>
2023-03-07 22:32:24 +0300

    Added FluentValidationAttribute

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -10,14 +10,12 @@ namespace NLayer.API.Controllers
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
-        private readonly IProductService _productService;
+        private readonly IProductService _service;
 
         public ProductsController(IMapper mapper, IService<Product> service, IProductService productService)
         {
             _mapper = mapper;
-            _service = service;
-            _productService = productService;
+            _service = productService;
         }
 
 
@@ -28,7 +26,7 @@ namespace NLayer.API.Controllers
         {
             //var res = await _productService.GetProductsWithCategory();
             //aynı
-            return CreateActionResult(await _productService.GetProductsWithCategory());
+            return CreateActionResult(await _service.GetProductsWithCategory());
         }
 
         //GET --> www.mysite.com/api/products => tum productlar
--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,4 +1,7 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NLayer.API.Filters;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -7,13 +10,25 @@ using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
 using NLayer.Service.Mapping;
 using NLayer.Service.Services;
+using NLayer.Service.Validations;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//Validator olusturduk ve hatalarý tamnýmladýk. Global olsun diye options ile tum validate leri gezip aktive ettik
+builder.Services.AddControllers(options => options.Filters.Add(new ValidateFilterAttribute())).AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+
+//Framework ayarlarýný degistirmek icin method
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    //Framework un default degelen filter ini baskýladýk. inaktive ettik
+    //Bu sayede artýk bizim yazdýgýmýz ValidateFilterAttribute classýndaki filter methodu calýsacak
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
