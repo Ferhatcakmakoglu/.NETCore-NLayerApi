80ca71b4dca66690780255b07e1b87bfd7a4e446
Ferhat Cakmakoglu <ferhat.cakmakoglu61@gmail.com>
2023-06-02 16:46:02 +0300

    FluentValidation Defined (Base: Services.Validations)

--- NLayer.Service/Validations/ProductDtoValidator.cs
+++ NLayer.Service/Validations/ProductDtoValidator.cs
@@ -16,7 +16,7 @@ namespace NLayer.Service.Validations
             // Decimal degerin default u 0 old. icin ona range koyduk!
             RuleFor(x => x.Price).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
             RuleFor(x => x.Stock).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
-            RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
+            //RuleFor(x => x.CategoryId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0");
         }
     }
 }
